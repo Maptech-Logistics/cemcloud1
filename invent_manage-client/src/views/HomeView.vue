10a2ebc9805b1f1d1dbf0fca60910bce37ced94a
Simon-157 <boatengsimonjnr157@gmail.com>
2023-07-10 18:43:10 +0000

    setup: initiaized vue project for the client side

---
+++ invent_manage-client/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeadLine from '../components/HeadLine.vue'
+
+
+</script>
+
+<template>
+  <main>
+    <HeadLine msg="Welcome to the Maptec logistics inventory management dashboard"/>
+  </main>
+</template>
