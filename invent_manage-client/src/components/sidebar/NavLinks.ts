6c6b5f50ddd660af1666c8f2a071b2e9f57eddcf
Simon-157 <boatengsimonjnr157@gmail.com>
2023-07-12 17:45:05 +0000

    refactor:refactored the components that uses specific types. types and interfaces moved to types directory

--- invent_manage-client/src/components/sidebar/NavLinks.ts
+++ invent_manage-client/src/components/sidebar/NavLinks.ts
@@ -1,14 +1,9 @@
 import SvgHomeIcon from '@/components/icons/HomeIcon.vue'
-import SvgLogoutIcon from '@/components/icons/LogoutIcon.vue'
 import SvgDashboardIcon from '@/components/icons/DashboardIcon.vue'
 import SvgAnalyticsIcon from '@/components/icons/AnalyticsIcon.vue'
 import SvgProfileIcon from '@/components/icons/ProfileIcon.vue'
+import type { Link } from '@/types/types'
 
-interface Link {
-  to: string
-  icon: any
-  text: string
-}
 
 export const navLinks: Link[] = [
   { to: '/', icon: SvgHomeIcon, text: 'Home' },
