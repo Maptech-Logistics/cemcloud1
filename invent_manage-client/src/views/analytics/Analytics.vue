dc894953fa797d77ff4e45b4472edce5dd74d8ac
Simon Junior Boateng <79936608+Simon-157@users.noreply.github.com>
2023-07-14 14:39:17 +0000

    Revert "Feature/delete inventory"

--- invent_manage-client/src/views/analytics/Analytics.vue
+++
@@ -1,7 +0,0 @@
-<template lang="">
-  <h2>this is the analytics page</h2>
-</template>
-<script>
-export default {}
-</script>
-<style lang=""></style>
--- invent_manage-client/src/views/profile/Profile.vue
+++
@@ -1,5 +0,0 @@
-<template lang="">
-  <h2>This is the profile page</h2>
-</template>
-
-<style scoped lang="scss"></style>
